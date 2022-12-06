5b81a18be8b07af5a7c2bb8d9af7f0d45c5aa0da
morphinese <xa13275685@student.karazin.ua>
2022-12-06 22:57:16 +0200

    w

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const par = document.getElementById('answer')
 btn.addEventListener('click', answer)
 
 function answer() {
-    if (y1.checked) {
+    if (y2.checked) {
         right_var.classList.remove('hidden')
         wrong_var.classList.add('hidden')
         par.classList.add('hidden')
